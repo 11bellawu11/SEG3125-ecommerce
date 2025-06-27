d9dfc19691a14d86191111fc05365d781d88d068
Enuma <136878147+Enuma04@users.noreply.github.com>
2025-06-27 14:28:16 -0400

    gameslist + homepage

---
+++ seg3125-ecommerce/src/pages/GameList.js
@@ -0,0 +1,88 @@
+export const games = [
+  {
+    id: 1,
+    title: "Overcooked! 2",
+    price: 9.99,
+    type: "Casual",
+    image: "/images/e-commerce.avif"
+  },
+  {
+    id: 2,
+    title: "Fall Guys",
+    price: 7.99,
+    type: "Casual",
+    image: "/images/e-commerce.avif"
+  },
+  {
+    id: 3,
+    title: "Among Us",
+    price: 4.99,
+    type: "Casual",
+    image: "/images/e-commerce.avif"
+  },
+  {
+    id: 4,
+    title: "Gang Beasts",
+    price: 8.49,
+    type: "Casual",
+    image: "/images/e-commerce.avif"
+  },
+
+  {
+    id: 5,
+    title: "The Legend of Zelda: Breath of the Wild",
+    price: 29.99,
+    type: "Adventure",
+    image: "/images/e-commerce.avif"
+  },
+  {
+    id: 6,
+    title: "Uncharted 4: A Thief’s End",
+    price: 19.99,
+    type: "Adventure",
+    image: "/images/e-commerce.avif"
+  },
+  {
+    id: 7,
+    title: "Tomb Raider",
+    price: 14.99,
+    type: "Adventure",
+    image: "/images/e-commerce.avif"
+  },
+  {
+    id: 8,
+    title: "Life is Strange",
+    price: 12.49,
+    type: "Adventure",
+    image: "/images/e-commerce.avif"
+  },
+
+  {
+    id: 9,
+    title: "Stardew Valley",
+    price: 13.99,
+    type: "Simulation",
+    image: "/images/e-commerce.avif"
+  },
+  {
+    id: 10,
+    title: "The Sims 4",
+    price: 16.99,
+    type: "Simulation",
+    image: "/images/e-commerce.avif"
+  },
+  {
+    id: 11,
+    title: "Cities: Skylines",
+    price: 11.99,
+    type: "Simulation",
+    image: "/images/e-commerce.avif"
+  },
+  {
+    id: 12,
+    title: "Animal Crossing: New Horizons",
+    price: 24.99,
+    type: "Simulation",
+    image: "/images/e-commerce.avif"
+  }
+];
---
+++ seg3125-ecommerce/src/pages/Home.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import { games } from './GameList';
+
+function Home() {
+  return (
+    <h1>Home</h1>    
+  );
+}
+export default Home;
