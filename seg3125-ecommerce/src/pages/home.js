1bc24a1d2b288b4e4117f9a373789034b1fbedbb
Enuma <136878147+Enuma04@users.noreply.github.com>
2025-06-27 20:57:40 -0400

    changes

--- seg3125-ecommerce/src/pages/home.js
+++ seg3125-ecommerce/src/pages/home.js
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import './home.css';
 import { Link } from 'react-router-dom';
+import { games } from './GameList';
 
 function Home() {
   return(
