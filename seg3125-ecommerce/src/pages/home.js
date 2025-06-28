7f6d84f1d459b88393bc615b566665f9d7b0b785
bella wu <qwu025@uottawa.ca>
2025-06-27 20:46:55 -0400

    created header component

---
+++ seg3125-ecommerce/src/pages/home.js
@@ -0,0 +1,12 @@
+import Container from 'react-bootstrap/Container';
+import './home.css';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return(
+    <div className="body">
+      
+    </div>
+  );
+}
+export default Home;
