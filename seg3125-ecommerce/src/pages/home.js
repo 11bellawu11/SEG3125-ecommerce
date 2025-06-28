2bd60dca86ee6c84127846b54d31eb3024942ad1
Enuma <136878147+Enuma04@users.noreply.github.com>
2025-06-27 21:07:15 -0400

    home restore

---
+++ seg3125-ecommerce/src/pages/home.js
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+import './home.css';
+import { Link } from 'react-router-dom';
+import { games } from './GameList';
+
+function Home() {
+  return(
+    <div className="body">
+      
+    </div>
+  );
+}
+export default Home;
